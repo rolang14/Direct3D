788a6e060caf5e0230602a258a37fc0b557cb3ab
rolang14 <rolang14@naver.com>
2022-10-02 17:51:12 +0900

    002강

---
+++ Engine002/Engine002.cpp
@@ -0,0 +1,30 @@
+﻿#include "SystemClass.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+{
+	SystemClass* System;
+	bool result;
+
+	//System 객체 생성
+	System = new SystemClass;
+	if (!System)
+	{
+		return 0;
+	}
+
+	//System 객체를 초기화하고 Run 호출
+	result = System->Initialize();
+	//제대로 초기화되면
+	if (result)
+	{
+		//Run
+		System->Run();
+	}
+
+	//유저가 종료하여 Run 이 종료되면 Shutdown 하고 객체 삭제, 메모리 반환
+	System->Shutdown();
+	delete System;
+	System = 0;
+
+	return 0;
+}
---
+++ Engine002/GraphicsClass.cpp
@@ -0,0 +1,38 @@
+#include "GraphicsClass.h"
+
+//나중에 내용들이 채워져야 함.
+GraphicsClass::GraphicsClass()
+{
+}
+
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+}
+
+GraphicsClass::~GraphicsClass()
+{
+}
+
+bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+
+	return true;
+}
+
+void GraphicsClass::Shutdown()
+{
+
+	return;
+}
+
+bool GraphicsClass::Frame()
+{
+
+	return true;
+}
+
+bool GraphicsClass::Render()
+{
+
+	return true;
+}
